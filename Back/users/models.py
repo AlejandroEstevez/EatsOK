2c1d1883da30f906db4d78e54ced503220b314fd
AlejandroEstevez <alexesmo78@gmail.com>
2026-08-14 19:51:28 +0200

    Initial backend setup

---
+++ Back/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        CLIENT = "CLIENT", "Client"
+        OWNER = "OWNER", "Owner"
+        ADMIN = "ADMIN", "Administrator"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CLIENT,
+    )
